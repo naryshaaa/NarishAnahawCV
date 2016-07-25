b896b545d91905bd1229ea16e5300e762c1b7dcf
Narish Anahaw <jhannarhizze@gmail.com>
2016-07-25 16:18:55 +0800

    added experience

---
+++ public/javascripts/spinners.js
@@ -0,0 +1,88 @@
+
+
+$(document).ready(function() {
+
+    $('.loader').ClassyLoader({
+
+        width: 200, // width of the loader in pixels
+        height: 200, // height of the loader in pixels
+        animate: true, // whether to animate the loader or not
+        displayOnLoad: true,
+        percentage: 90, // percent of the value, between 0 and 100
+        speed: 100, // miliseconds between animation cycles, lower value is faster
+        roundedLine: false, // whether the line is rounded, in pixels
+        showRemaining: true, // how the remaining percentage (100% - percentage)
+        fontFamily: 'Helvetica', // name of the font for the percentage
+        fontSize: '50px', // size of the percentage font, in pixels
+        showText: true, // whether to display the percentage text
+        diameter: 80, // diameter of the circle, in pixels
+        fontColor: 'rgba(25, 25, 25, 0.6)', // color of the font in the center of the loader, any CSS color would work, hex, rgb, rgba, hsl, hsla
+        lineColor: 'rgba(0,0,255, 0.5)', // line color of the main circle
+        remainingLineColor: 'rgba(55, 55, 55, 0.4)', // line color of the remaining percentage (if showRemaining is true)
+        lineWidth: 5 // the width of the circle line in pixels
+
+    });
+
+     $('.loader1').ClassyLoader({
+
+            width: 200, // width of the loader in pixels
+            height: 200, // height of the loader in pixels
+            animate: true, // whether to animate the loader or not
+            displayOnLoad: true,
+            percentage: 90, // percent of the value, between 0 and 100
+            speed: 100, // miliseconds between animation cycles, lower value is faster
+            roundedLine: false, // whether the line is rounded, in pixels
+            showRemaining: true, // how the remaining percentage (100% - percentage)
+            fontFamily: 'Helvetica', // name of the font for the percentage
+            fontSize: '50px', // size of the percentage font, in pixels
+            showText: true, // whether to display the percentage text
+            diameter: 80, // diameter of the circle, in pixels
+            fontColor: 'rgba(25, 25, 25, 0.6)', // color of the font in the center of the loader, any CSS color would work, hex, rgb, rgba, hsl, hsla
+            lineColor: 'rgba(0,0,255, 0.5)', // line color of the main circle
+            remainingLineColor: 'rgba(55, 55, 55, 0.4)', // line color of the remaining percentage (if showRemaining is true)
+            lineWidth: 5 // the width of the circle line in pixels
+
+        });
+      $('.loader2').ClassyLoader({
+
+             width: 200, // width of the loader in pixels
+             height: 200, // height of the loader in pixels
+             animate: true, // whether to animate the loader or not
+             displayOnLoad: true,
+             percentage: 80, // percent of the value, between 0 and 100
+             speed: 100, // miliseconds between animation cycles, lower value is faster
+             roundedLine: false, // whether the line is rounded, in pixels
+             showRemaining: true, // how the remaining percentage (100% - percentage)
+             fontFamily: 'Helvetica', // name of the font for the percentage
+             fontSize: '50px', // size of the percentage font, in pixels
+             showText: true, // whether to display the percentage text
+             diameter: 80, // diameter of the circle, in pixels
+             fontColor: 'rgba(25, 25, 25, 0.6)', // color of the font in the center of the loader, any CSS color would work, hex, rgb, rgba, hsl, hsla
+             lineColor: 'rgba(0,0,255, 0.5)', // line color of the main circle
+             remainingLineColor: 'rgba(55, 55, 55, 0.4)', // line color of the remaining percentage (if showRemaining is true)
+             lineWidth: 5 // the width of the circle line in pixels
+
+         });
+      $('.loader3').ClassyLoader({
+
+             width: 200, // width of the loader in pixels
+             height: 200, // height of the loader in pixels
+             animate: true, // whether to animate the loader or not
+             displayOnLoad: true,
+             percentage: 80, // percent of the value, between 0 and 100
+             speed: 100, // miliseconds between animation cycles, lower value is faster
+             roundedLine: false, // whether the line is rounded, in pixels
+             showRemaining: true, // how the remaining percentage (100% - percentage)
+             fontFamily: 'Helvetica', // name of the font for the percentage
+             fontSize: '50px', // size of the percentage font, in pixels
+             showText: true, // whether to display the percentage text
+             diameter: 80, // diameter of the circle, in pixels
+             fontColor: 'rgba(25, 25, 25, 0.6)', // color of the font in the center of the loader, any CSS color would work, hex, rgb, rgba, hsl, hsla
+             lineColor: 'rgba(0,0,255, 0.5)', // line color of the main circle
+             remainingLineColor: 'rgba(55, 55, 55, 0.4)', // line color of the remaining percentage (if showRemaining is true)
+             lineWidth: 5 // the width of the circle line in pixels
+
+         });
+});
+
+
